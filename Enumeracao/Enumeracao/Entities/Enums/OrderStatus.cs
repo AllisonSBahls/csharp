b8c20fa82743b165740aa47d4a06a9b1b0db4381
AllisonSBahls <allison_sousa_bahls@hotmail.com>
2020-12-06 17:49:56 -0400

    Usando enumerações para solicitar pedido

---
+++ Enumeracao/Enumeracao/Entities/Enums/OrderStatus.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Enumeracao.Entities.Enums
+{
+    //Criando uma Enumeração para o Order Status
+    enum OrderStatus : int //indicando que o enum é inteiro
+    {
+        //Colando os valores possiveis do meu tipo enumerado e definido o valor que represente ele
+        PendingPayment = 0,
+        Processing = 1,
+        Shipped = 2,
+        Delivered = 3
+    }
+}
---
+++ Enumeracao/Enumeracao/Program.cs
@@ -0,0 +1,33 @@
+﻿using Enumeracao.Entities;
+using Enumeracao.Entities.Enums;
+using System;
+namespace Enumeracao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Order order = new Order
+            {
+                Id = 1080,
+                Moment = DateTime.Now,
+
+                //Designando a ordem desejada ao meu status usando o enum
+                Status = OrderStatus.PendingPayment
+
+            };
+            Console.WriteLine(order);
+
+            //Conversão de STRING para ENUM;
+            string txt = OrderStatus.PendingPayment.ToString();
+            Console.WriteLine(txt);
+
+            //Conversão de ENUM para STRING;
+            // Definido o tipo do enum que recebera a conversão e declarado seu tipo de conversão. 
+            //entre parenteses deve estar o Status que deseja converter, lebrando que deve ser igual ao nome;
+            OrderStatus os = Enum.Parse<OrderStatus>("Delivered");
+
+            Console.WriteLine(os);
+        }
+    }
+}
