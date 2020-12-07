cea6c5bef571942c09e30f1c0eed3d759b6c254f
Allison Bahls <allison_sousa_bahls@hotmail.com>
2020-12-07 15:02:52 -0400

    Usando polimorfismo

---
+++ Polimorfismo/Polimorfismo/Entities/OutersourcedEmployee.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Polimorfismo.Entities
+{
+    //Herdando as propriedades e metodos da classe employee
+    class OutersourcedEmployee : Employee
+    {
+        //Acrescentar o que ele tem a mais que a super classe
+        public double AdditionalCharge { get; set; }
+
+        public OutersourcedEmployee()
+        {
+
+        }
+
+        //Reaproveitando os construtores da super classe
+        public OutersourcedEmployee(string name, int hours, double valuePerHour, double additionalCharge) : base(name, hours, valuePerHour)
+        {
+            AdditionalCharge = additionalCharge;
+        }
+
+        public override double Payment()
+        {
+            return base.Payment() + 1.10 * AdditionalCharge;
+        }
+    }
+}
---
+++ Polimorfismo/Polimorfismo/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using Polimorfismo.Entities;
+using System.Globalization;
+namespace Polimorfismo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Chamando uma lista do tipo Employee(Funcionario)
+            List<Employee> list = new List<Employee>();
+            Console.Write("Enter the number of employee: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"Employee #{i} data:");
+                Console.Write("Outsourced (y/n)> ");
+                char ch = char.Parse(Console.ReadLine());
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Hours: ");
+                int hours = int.Parse(Console.ReadLine());
+                Console.Write("Value per Hour: ");
+                double valuePerHour = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            
+                if (ch == 'y')
+                {
+                    Console.WriteLine("Additional charge: ");
+                    double additionalCharge = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    //Basicamente aqui já temos caso de polimorfismo, pois a lista do tipo Employee esta recebendo dados de uma  outra classe a qual é sua subclasse
+                    list.Add(new OutersourcedEmployee(name, hours, valuePerHour, additionalCharge));
+                }
+                else
+                {
+                    //E aqui ele recebe dados da classe
+                    list.Add(new Employee(name, hours, valuePerHour));
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine("PAYMENTS: ");
+            foreach (Employee employee in list)
+            {
+                Console.WriteLine(employee.Name + " - $ " + employee.Payment().ToString("F2", CultureInfo.InvariantCulture));
+            }
+        }
+    }
+}
