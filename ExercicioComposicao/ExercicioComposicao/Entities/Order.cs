4c1d7834f4b4909356307aa66d7f66a78653a60f
AllisonSBahls <allison_sousa_bahls@hotmail.com>
2020-12-06 22:55:45 -0400

    Exercicios usando Composição, Enumeração e StringBuilder

---
+++ ExercicioComposicao/ExercicioComposicao/Entities/Order.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+using ExercicioComposicao.Entities.Enums;
+
+namespace ExercicioComposicao.Entities
+{
+    class Order
+    {
+        public DateTime Moment { get; set; }
+        //O status do pedido é um enum 
+        public OrderStatus Status { get; set; }
+        //Como são varios pedidos serão armazenados em uma lista e já esta inicializada por garantia
+        public List<OrderItem> Items { get; set; } = new List<OrderItem>();
+
+        public Client Client { get; set; }
+        public Order() { }
+
+        //Items não preisam ser colocado no construtor
+        public Order(DateTime moment, OrderStatus status, Client client)
+        {
+            Moment = moment;
+            Status = status;
+            Client = client;
+        }
+
+        public void AddItem(OrderItem item)
+        {
+            Items.Add(item);
+        }
+
+        public void RemoveItem(OrderItem item)
+        {
+            Items.Remove(item);
+        }
+
+        public double Total()
+        {
+            double sum = 0;
+            foreach (OrderItem item in Items)
+            {
+                //sum += item.subTotal(); ou
+                sum = sum + item.SubTotal();
+            }
+            return sum;
+        }
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("ORDER SUMMARY: ");
+            sb.Append("Order moment: ");
+            sb.AppendLine(Moment.ToString("dd/MM/yyyy HH:mm:ss"));
+            sb.AppendLine("Client: " + Client);
+            sb.AppendLine("Order Items");
+            foreach (OrderItem item in Items)
+            {
+                sb.AppendLine(item.ToString());
+            }
+            sb.Append("Total price: ");
+            sb.Append(Total());
+            return sb.ToString();
+            
+        }
+    }
+}
---
+++ ExercicioComposicao/ExercicioComposicao/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Globalization;
+using ExercicioComposicao.Entities;
+using ExercicioComposicao.Entities.Enums;
+
+namespace ExercicioComposicao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter cliente data: ");
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Email: ");
+            string email = Console.ReadLine();
+
+            Console.Write("Birth Date (DD/MM/YYYY): ");
+            DateTime birthDate = DateTime.Parse(Console.ReadLine());
+
+
+            Client client = new Client(name, email, birthDate);
+
+
+            Console.WriteLine();
+            Console.WriteLine("Enter order data: ");
+            Console.Write("Status: ");
+            OrderStatus orderStatus = Enum.Parse<OrderStatus>(Console.ReadLine());
+
+            Order order = new Order(DateTime.Now, orderStatus, client);
+
+            Console.Write("How many item to this order? : ");
+            int qtdOrder = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= qtdOrder; i++) 
+            {
+                Console.WriteLine($"Enter {i} item data:");
+                Console.Write("Product name: ");
+                string nameProduct = Console.ReadLine();
+                Console.Write("Product price: ");
+                double priceProduct = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.Write("Quantity: ");
+                int quantity = int.Parse(Console.ReadLine());
+
+                Product product = new Product(nameProduct, priceProduct);
+                
+                OrderItem items = new OrderItem(quantity, priceProduct, product);
+                order.AddItem(items);
+            }
+            Console.WriteLine();
+            Console.WriteLine(order);
+        }
+    }
+}
