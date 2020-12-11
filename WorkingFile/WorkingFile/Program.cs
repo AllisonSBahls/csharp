38bfdb45dab74f3c0b29fe1d604a6eb55d59319a
AllisonSBahls <allisonsousabahlsas@gmail.com>
2020-12-11 15:41:45 -0400

    starting to work files

---
+++ WorkingFile/WorkingFile/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+namespace WorkingFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Buscando o arquivo criado - é usado o @ para não precisar digitar \\
+            string sourcePath = @"c:\temp\file1.txt";
+            string targePath = @"c:\temp\file2.txt";
+            try
+            {
+                //Instancia o objeto que estará associado ao arquivo que existir no caminhoq ue for passado que no caso é o sourcePath
+                FileInfo fileInfo = new FileInfo(sourcePath);
+
+                //Ler todas as linhas do arquivo e guardar em um vetor passando nele o arquivo
+                string[] lines = File.ReadAllLines(sourcePath);
+                foreach (string line in lines)
+                {
+                    Console.WriteLine(line);
+                }
+
+                //Copiando para o arquivo instanciado para o local destino
+                fileInfo.CopyTo(targePath);
+            }
+
+            catch(IOException e)
+            {
+                Console.WriteLine("An error occured: " + e.Message);
+            }
+        }
+    }
+}
