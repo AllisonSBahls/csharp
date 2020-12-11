f3bb85d3561eb19f26d2e07a65910341bb59af14
AllisonSBahls <allisonsousabahlsas@gmail.com>
2020-12-11 16:28:48 -0400

    Using

---
+++ WorkingFile/WorkingFile/BlockUsing.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace WorkingFile
+{
+    class BlockUsing
+    {
+        public void Using()
+        {
+            string path = @"c:\temp\file1.txt";
+            try
+            {
+                //Tudo que estiver nesse bloco sera executado e instantaneamente fechado
+                /*using (FileStream fs = new FileStream(path, FileMode.Open))*/
+                //{
+                //Pode ter varios blocos using
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string line = sr.ReadLine();
+                        Console.WriteLine(line);
+                    }
+                }
+                //}
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error Ocurred: " + e.Message);
+            }
+        }
+    }
+}
--- WorkingFile/WorkingFile/Program.cs
+++ WorkingFile/WorkingFile/Program.cs
@@ -1,16 +1,27 @@
 ﻿using System;
 using System.IO;
+
+
 namespace WorkingFile
 {
     class Program
     {
+        /*Para instancia de FILE trabalhada como FileInfo, FileStream, Using entre outros
+         * foram criados classes separadas e nelas metódos para realizarem os testes com
+         * o objetivo de manter tudo em um mesmo projeto*/
         static void Main(string[] args)
         {
+           
             /*FileAndFileInfo file = new FileAndFileInfo();
             file.FilesFirst();
             */
-            FileStreamAndStreamReader fssr = new FileStreamAndStreamReader();
-            fssr.FileStream();
+           
+            /*FileStreamAndStreamReader fssr = new FileStreamAndStreamReader();
+            fssr.FileStream();*/
+
+            BlockUsing bu = new BlockUsing();
+            bu.Using();
+
         }
     }
 }
