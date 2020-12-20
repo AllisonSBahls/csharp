00b18f0ab3f09b298aa71ae5e4c219e85bc82dfe
Allison Bahls <allison_sousa_bahls@hotmail.com>
2020-12-20 19:07:59 -0300

    Atividades usando interface

---
+++ InterfaceAtividade/InterfaceAtividade/Services/ContractService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+using System.Text;
+
+using InterfaceAtividade.Entities;
+
+namespace InterfaceAtividade.Services
+{
+    class ContractService
+    {
+        private IOnlinePaymentService _onlinePaymentService;
+
+        public ContractService(IOnlinePaymentService onlinePaymentService)
+        {
+            _onlinePaymentService = onlinePaymentService;
+        }
+
+        public void ProcessContract(Contract contract, int months)
+        {
+            double basicQuota = contract.TotalValue / months;
+            for (int i = 0; i < months; i++)
+            {
+                DateTime date = contract.Date.AddMonths(i);
+                double updateQuota = basicQuota + _onlinePaymentService.Interest(basicQuota, i);
+                double quota = updateQuota + _onlinePaymentService.PaymentFee(updateQuota);
+                contract.AddInstallment(new Installment(date, quota));
+            }
+           
+        }
+
+    }
+}
---
+++ InterfaceAtividade/InterfaceAtividade/Services/PayPalService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using InterfaceAtividade.Entities;
+
+namespace InterfaceAtividade.Services
+{
+    class PayPalService : IOnlinePaymentService
+    {
+        public double Interest(double amount, int months)
+        {
+                return  amount * 0.01 * months;
+
+        }
+
+        public double PaymentFee(double amount)
+        {
+            return amount * 0.02;
+        }
+    }
+}
