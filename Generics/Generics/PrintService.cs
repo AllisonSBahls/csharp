4b7dc5ddfd70163eb233c9c82fc32cab6840e7df
Allison Bahls <allison_sousa_bahls@hotmail.com>
2020-12-20 19:06:39 -0300

    Definindo o tipo genérico

---
+++ Generics/Generics/PrintService.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Generics
+{
+    //Definindo como generico do tipo "T"
+    class PrintService <T>
+    {
+        //variaveis interna usasse _
+        //Variavel internet que armazena 10 itens iteiros
+        private T[] _values = new T[10];
+
+        private int _count = 0;
+
+        public void AddValue(T value)
+        {
+            //Programação defensiva
+            if(_count == 10)
+            {
+                throw new InvalidOperationException("Print Service is full");
+            }
+            _values[_count] = value;
+            
+            //atualizar o vetor _count
+            _count++;
+        }
+
+        public T First()
+        {
+            //Programação defensiva
+            if (_count == 0)
+            {
+                throw new InvalidOperationException("Print Service is empty");
+            }
+            return _values[0];
+        }
+
+       public void Print()
+        {
+            Console.Write("[ ");
+            for (int i = 0; i < _count - 1; i++)
+            {
+                Console.Write(_values[i] + ", ");
+            }
+            if(_count > 0)
+            {
+                Console.Write(_values[_count - 1]);
+            }
+            Console.WriteLine("]");
+        }
+    }
+}
---
+++ Generics/Generics/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Dessa forma sera na instanciação que sera definido o tipo dela
+            PrintService<int> printService = new PrintService<int>();
+            Console.Write("How many values? ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                int x = int.Parse(Console.ReadLine());
+                printService.AddValue(x);
+            }
+
+            printService.Print();
+            Console.WriteLine("First: " + printService.First());
+        }
+    }
+}
