42da2adbd796f8e1a4adc2f9bec5832c16608a15
Allison Bahls <allison_sousa_bahls@hotmail.com>
2020-12-20 19:07:36 -0300

    Usando o GetHashCode para melhor desempenho e o equals para validar

---
+++ HashCodeAndEquals/HashCodeAndEquals/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Entities;
+namespace HashCodeAndEquals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Client a = new Client { Name = "Maria", Email = "maria@gmail.com" };
+            Client b = new Client { Name = "Allison", Email = "allison@gmail.com" };
+
+            Console.WriteLine(a.Equals(b));
+          
+            Console.WriteLine(a.GetHashCode());
+            Console.WriteLine(b.GetHashCode());
+        }
+    }
+}
